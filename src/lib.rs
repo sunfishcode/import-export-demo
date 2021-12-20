d657bc368fa77d411ab68e3eacedd2676cf2809b
Dan Gohman <dev@sunfishcode.online>
2021-12-20 13:50:29 -0800

    Fix compilation errors.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 wit_bindgen_rust::import!("provider.wit");
 wit_bindgen_rust::export!("runner.wit");
 
-struct Exports;
+struct Runner;
 
-impl runner::Runner for Exports {
-    fn doit(it: provider::Thing) {
+impl runner::Runner for Runner {
+    fn doit(it: wit_bindgen_rust::Handle<provider::Thing>) {
         it.frob()
     }
 }
